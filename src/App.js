eadce87ef3f61d81c78a47799d1f1f9505a55ea3
Rahul Motan <rahulmotan01@gmail.com>
2018-06-25 02:11:35 -0400

    gitignore

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import RegisterContainer from './containers/Register'
 import TrialContainer from './containers/Trials'
 import NavbarContainer from './containers/NavBar'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+import DashboardContainer from './containers/Dashboard'
+import Carousel from './components/carousel'
 
 class App extends React.Component {
     render() {
@@ -10,10 +12,14 @@ class App extends React.Component {
             <Router>
                 <section>
                     <NavbarContainer/>
+                    <Route exact path={'/'} component={Carousel}/>
                     <section className="container-fluid">
                         <Route path='/register' component={RegisterContainer}/>
                         <section className="container">
                             <Route path='/search/:searchText/results' component={TrialContainer}/>
+                        </section>
+                        <section>
+                            <Route path={'/home/pa/dashboard'} component={DashboardContainer}/>
                         </section>
                     </section>
                 </section>
