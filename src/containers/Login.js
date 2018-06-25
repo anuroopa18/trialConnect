abb85940d59b89f88799f92c32edb2db1fcc6d1c
anuroopa18 <tsanuroopa@gmail.com>
2018-06-25 13:08:41 -0400

    UI changes for login register profile

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from "react-redux";
 import * as actions from '../actions/Actions'
+import logo from '../assets/TrialLogo2.jpeg.png'
 
 const stateToPropertiesMapper = ({loginReducer}) => {
 
@@ -27,54 +28,74 @@ const Login = ({loginUsername,loginPassword,loginRole,login,role}) => {
     let roleInputDoctor;
 
     return (
-        <div className="col-md-6 col-md-offset-3">
-            <h1>Login</h1>
-            
-               
+        <div className="row">
+        <div className="col-md-4 ">
+        </div>
+        
+        <div className="col-md-4 ">
+            <div className="card">
+            <div className="card-header">
+            <h1 style={{"textAlign":"center","color":"#2994b2","fontWeight":"bold"}}>Login</h1>
+            </div>
+            <div class="card-body">
+            <div style={{"justifyContent": "center","alignItems": "center"}}>
+            <img className="center" src={logo} width="60" height="60"/>
+            </div>
                 <div>
-                    <label htmlFor="username">Username</label>
+                    <label htmlFor="username" style={{"color":"#2994b2"}}>Username</label>
                     <input type="text" className="form-control" name="username" ref={node => usernameInput = node}
                            onChange={() => {
                             loginUsername(usernameInput.value)
                            }}/>
                 </div>
                 <div>
-                    <label htmlFor="password">Password</label>
+                    <label htmlFor="password" style={{"color":"#2994b2"}}>Password</label>
                     <input type="password" className="form-control" name="password" ref={node => passwordInput = node}
                            onChange={() => {
                             loginPassword(passwordInput.value)
                            }}/>
                 </div>
                 <p></p>
-                <label>Login as</label>
-
-                    <div className="radio">
-                        <label>
-                            <input type="radio" value="Doctor" checked={role === "Doctor"}
+                <label htmlFor="radio" style={{"color":"#2994b2"}}>Login as</label>
+                 <div className="flex-row">
+                    <div className="input-group">
+                        <label  style={{"color":"#2994b2","marginRight":"10px"}}>
+                            <input   type="radio" value="Doctor" checked={role === "Doctor"}
                                    ref={node => roleInputDoctor = node} onChange={() => {
                                     loginRole(roleInputDoctor.value)
                             }}/>
                             Doctor
                         </label>
-                    </div>
-                    <div className="radio">
-                        <label>
-                            <input type="radio" value="Patient" checked={role === "Patient"}
+                    
+                   
+                        <label style={{"color":"#2994b2"}}>
+                            <input type="radio"  value="Patient" checked={role === "Patient"}
                                    ref={node => roleInputPatient = node} onChange={() => {
                                     loginRole(roleInputPatient.value)
                             }}/>
                             Patient
                         </label>
+                        </div>
                     </div>
+                    
                 
 
                 <div className="form-group">
-                    <button className="btn btn-primary" onClick={() => {
+                    <button className="btn btn-block btn-lg" style={{"backgroundColor":"#22a7cc","color":"white"}} onClick={() => {
                         login()
                     }}>Login
                     </button>
 
-                </div>           
+                </div>
+                <div className="card-footer">
+                <p>&nbsp;</p>
+                </div>
+                </div>    
+                </div>       
+        </div>
+        <div className="col-md-4 ">
+       
+        </div>
         </div>
     )
 };
